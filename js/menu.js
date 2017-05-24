493cb63807961511eacd0e712ba86378e6f4bacf
anneliesvanminsel <anneliesvanminsel@outlook.com>
2017-05-24 16:25:51 +0200

    menu werkt!
    Danku Lionel

--- js/menu.js
+++ js/menu.js
@@ -1,43 +1,25 @@
-//$(".cross").hide();
-//$(".menu").hide();
-//$(".hamburger").click(function () {
-//  $(".menu").slideToggle("slow", function () {
-//    $(".hamburger").hide();
-//    $(".cross").show();
-//  });
-//});
-//$(".cross").click(function () {
-//  $(".menu").slideToggle("slow", function () {
-//    $(".cross").hide();
-//    $(".hamburger").show();
-//  });
-//});
-// <nav class="menu">
-//    <ul>
-//      <a href="dashboard.html">
-//        <li>Dashboard</li>
-//      </a>
-//      <a href="profiel.html">
-//        <li>Mijn profiel</li>
-//      </a>
-//      <a href="">
-//        <li>Leaderboard</li>
-//      </a>
-//      <a href="reward.html">
-//        <li>Mijn rewards</li>
-//      </a>
-//      <a href="media.html">
-//        <li>Mijn media</li>
-//      </a>
-//      <a href="faq.html">
-//        <li>FAQ</li>
-//      </a>
-//      <a href="#">
-//        <li>Instellingen</li>
-//      </a>
-//      <a href="about.html">
-//        <li>About</li>
-//      </a>
-//    </ul>
-//  </nav>
-document.getelementby
+var hamburger = document.getElementById('hamburger');
+var teller = 1;
+var mainObject = document.getElementsByTagName('main');
+mainObject = mainObject[0];
+hamburger.onclick = function () {
+  console.log('erin');
+  teller = teller + 1;
+  console.log(mainObject);
+  var write = '';
+  if (teller % 2 == 0) {
+    write += '<nav class="menu"><ul> <a href="dashboard.html"> <li> Dashboard </li></a>  <a href="profiel.html"> <li>Mijn profiel</li></a><a href="">  <li>Leaderboard</li></a><a href="reward.html">  <li>Mijn rewards</li></a><a href="media.html"><li>Mijn media</li></a><a href="faq.html"><li>FAQ</li></a><a href="#">   <li>Instellingen</li> </a><a href="about.html">  <li>About</li></a></ul></nav>';
+    console.log(write);
+    document.getElementById('uitgeklapte_menu').innerHTML = write;
+    mainObject.className = 'ingeklapt';
+    console.log(mainObject);
+  }
+  else if (teller % 2 == 1) {
+    console.log(mainObject);
+    document.getElementById('uitgeklapte_menu').innerHTML = '';
+    mainObject.className = 'uitgeklapt';
+  }
+  else {
+    document.getElementById('uitgeklapte_menu').innerHTML = "";
+  }
+};
