a41e1df779ea191b35c6a53835846f8a3f7ed1c9
Boris Ysenbaert <boris.ysenbaert@student.ehb.be>
2017-05-24 11:13:37 +0200

    reward

--- js/reward.js
+++ js/reward.js
@@ -1,3 +1,16 @@
 function claimReward() {
 
+  var child = document.getElementById("child");
+  var parent = document.getElementById("parent");
+  parent.removeChild(child);
+
+  var titel = document.createElement("p");
+  var tekst = document.createTextNode("Hieronder vind je jouw unieke code");
+  titel.appendChild(tekst);
+
+  parent.appendChild(titel);
+
+  parent.className = "titel";
+
+
 }
