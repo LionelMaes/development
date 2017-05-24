0c4ba93dc24b303ebea14320fdfc41ccf8d68d56
anneliesvanminsel <anneliesvanminsel@outlook.com>
2017-05-24 11:36:43 +0200

    lionel!

---
+++ js/menu.js
@@ -0,0 +1,43 @@
+//$(".cross").hide();
+//$(".menu").hide();
+//$(".hamburger").click(function () {
+//  $(".menu").slideToggle("slow", function () {
+//    $(".hamburger").hide();
+//    $(".cross").show();
+//  });
+//});
+//$(".cross").click(function () {
+//  $(".menu").slideToggle("slow", function () {
+//    $(".cross").hide();
+//    $(".hamburger").show();
+//  });
+//});
+// <nav class="menu">
+//    <ul>
+//      <a href="dashboard.html">
+//        <li>Dashboard</li>
+//      </a>
+//      <a href="profiel.html">
+//        <li>Mijn profiel</li>
+//      </a>
+//      <a href="">
+//        <li>Leaderboard</li>
+//      </a>
+//      <a href="reward.html">
+//        <li>Mijn rewards</li>
+//      </a>
+//      <a href="media.html">
+//        <li>Mijn media</li>
+//      </a>
+//      <a href="faq.html">
+//        <li>FAQ</li>
+//      </a>
+//      <a href="#">
+//        <li>Instellingen</li>
+//      </a>
+//      <a href="about.html">
+//        <li>About</li>
+//      </a>
+//    </ul>
+//  </nav>
+document.getelementby
