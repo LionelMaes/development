044b84a3c26f9939400d1bea07955f1586534a86
Boris Ysenbaert <boris.ysenbaert@student.ehb.be>
2017-05-26 00:16:45 +0200

    reward js

--- js/reward.js
+++ js/reward.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   $('button').click(function () {
-    $('#child').fadeOut('fast');
+    $('#child').hide('fast');
     $('#parent').append('<p>Hieronder vind je jouw unieke code</p>')
     $("p").addClass("code");
     $(".code").css("margin", "1em");
     $('#parent').append('<img src = "images/qrCode.png"/>')
-
-
-
+    $('#parent').append('<button id = "terug"onclick="terug()">Ga terug</button>');
+    $('#terug').css("margin-top", "1em");
+    $('#parent').fadeIn('fast');
   });
 });
+
+function terug() {
+  location.reload();
+}
